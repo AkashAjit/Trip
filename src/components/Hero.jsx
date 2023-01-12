765bc4e8dff929b8d5f01a50d6ebdaab26b4916b
Akash Ajith <akash.ajith3@gmail.com>
2023-01-12 21:23:13 +0530

    upto navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='w-full h-screen'>
-        <img className='top-0 left-0 w-full h-screen object-cover' src="https://images.pexels.com/photos/1267051/pexels-photo-1267051.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
-        <div className='bg-black/30 absolute top-0 left-0 w-full h-screen' />
-        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
-            <div>
-                <p>All Inclusive</p>
-                <h1>Private Beaches & Getaway</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptatem quo iure laboriosam neque id reprehenderit voluptatibus obcaecati facere modi?</p>
-                <button>Reserve Now</button>
-            </div>
+    <div className="w-full h-screen">
+      <img
+        className="top-0 left-0 w-full h-screen object-cover"
+        src="https://images.pexels.com/photos/1267051/pexels-photo-1267051.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt="/"
+      />
+      <div className="bg-black/30 absolute top-0 left-0 w-full h-screen" />
+      <div className="absolute top-0 w-full h-full flex flex-col justify-center text-white">
+        <div className="md: left-[10%] max-w-[1100px] m-auto absolute p-4">
+          <p>All Inclusive</p>
+          <h1 className="font-bold text-5xl md:text-7xl drop-shadow-5xl">
+            Private Beaches & Getaway
+          </h1>
+          <p className="max-w-[600px] drop-shadow-2xl py-2 text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+            voluptatem quo iure laboriosam neque id reprehenderit voluptatibus
+            obcaecati facere modi?
+          </p>
+          <button className="bg-white text-black">Reserve Now</button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
